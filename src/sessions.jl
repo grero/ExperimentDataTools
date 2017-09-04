4a0660d4d04c7b9dfa0fc71cbae380abe2703b8a
roger <roger.herikstad@gmail.com>
2017-09-04 14:45:44 +0800

    Adds code to create session hierarchy

--- src/sessions.jl
+++ src/sessions.jl
@@ -30,3 +30,58 @@ function save_session_data{T<:RawData}(H::T, data::Array{Float64,1}, session_mar
         save_data(H, session)
     end
 end
+
+"""
+Discovery git repository for the current path
+"""
+function discover_repo(path)
+    if isdir("$(path)/.git")
+        return LibGit2.GitRepo(path)
+    else
+        parts = split(path, "/")
+        if length(parts) > 1
+            return discover_repo(join(parts[1:end-1], "/"))
+        else
+            return nothing
+        end
+    end
+end
+
+"""
+Re-organize the current directory by moving session related files to their dedicated directories, e.g.
+w7_11_1.edf -> session01/w7_11_1.edf
+If files are under revision control, use git mv instead and opens the default editor asking for a commit message.
+"""
+function process()
+    repo = discover_repo(pwd())
+    repo_path = LibGit2.path(repo)*"/"
+    files = split(chomp(readstring(`find . -name "*_settings.txt"`)))
+    index_updated = false
+    for f in files
+        mm = match(r"([a-zA-Z]*)([0-9])_([0-9])_([0-9])",f)
+        animal,month,day,session = mm.captures
+        session = parse(Int64,session)
+        # find all files belonging to this session
+        sfiles = split(chomp(readstring(`find $(pwd()) -name "$(animal)$(month)_$(day)_$(session)*"`)))
+        session_name = @sprintf "session%02d" session
+        mkpath(session_name)
+        for sf in sfiles
+            if repo != nothing
+                qq = replace(sf, repo_path, "") # get the path relative to the repository
+                ss = LibGit2.status(repo, qq)
+            else
+                ss = Nullable{UInt32}()
+            end
+            if isnull(ss) # not under revision control
+                mv(sf, "$(session_name)/$sf")
+            else # under revision control
+                vv = replace(sf, pwd()*"/", "")
+                run(`git mv $(vv) $(session_name)/$vv`)
+                index_updated = true
+            end
+        end
+    end
+    if index_updated
+        run(`git commit`)
+    end
+end
