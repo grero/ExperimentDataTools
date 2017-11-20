9a2405f738d14c966af3aa4639d37eb756e69936
roger <roger.herikstad@gmail.com>
2017-11-20 18:10:20 +0800

    Adds code to plot reward and delay duration

--- src/training.jl
+++ src/training.jl
@@ -31,3 +31,61 @@ function plot_saccade_endpoints(fname::String)
     fig[:savefig](figfname)
     fig
 end
+
+function get_reward_duration()
+    files = split(readchomp(`find . -name "*results.txt"`), "\n")
+    reward_duration = Float64[]
+    delay = Float64[]
+    for f in files
+        results = readtable(f)
+        ridx  = find(results[:reward_on] .> 0)
+        append!(reward_duration, (results[:reward_off] - results[:reward_on])[ridx])
+        append!(delay, (results[:response_on] - results[:target_off])[ridx])
+    end
+    reward_duration, delay
+end
+
+function plot_reward_duration_distr()
+    reward_duration, delay = get_reward_duration()
+    plot_reward_duration(reward_duration, delay)
+end
+
+function plot_reward_duration_distr(reward_duration::Vector{Float64}, delay::Vector{Float64})
+    fig = plot_marginal_hist(reward_duration, delay)
+    ax = fig[:axes][1]
+    ax[:set_xlabel]("Reward duration [s]")
+    ax[:set_ylabel]("Delay duration [s]")
+    fig
+end
+
+function plot_marginal_hist(y1::Vector{Float64}, y2::Vector{Float64}) 
+    fig = plt[:figure]()
+    plot_marginal_hist(fig, y1, y2)
+    fig
+end
+
+function plot_marginal_hist(fig, y1::Vector{Float64}, y2::Vector{Float64}) 
+    left, width = 0.1, 0.65
+    bottom, height = 0.1, 0.65
+    bottom_h = left_h = left + width + 0.02
+
+    rect_scatter = [left, bottom, width, height]
+    rect_histy1 = [left, bottom_h, width, 0.2]
+    rect_histy2 = [left_h, bottom, 0.2, height]
+    
+    ax_scatter = fig[:add_axes](rect_scatter)
+    ax_histy1 = fig[:add_axes](rect_histy1)
+    ax_histy2 = fig[:add_axes](rect_histy2)
+    
+    h1 = KernelDensity.kde_lscv(y1)
+    h2 = KernelDensity.kde_lscv(y2)
+    ax_histy1[:plot](h1.x, h1.density)
+    ax_histy2[:plot](h2.density, h2.x)
+    ax_scatter[:scatter](y1,y2)
+    ax_histy1[:set_xlim](extrema(h1.x))
+    ax_histy1[:set_xticklabels]([])
+    ax_scatter[:set_xlim](extrema(h1.x))
+    ax_histy2[:set_ylim](extrema(h2.x))
+    ax_histy2[:set_yticklabels]([])
+    ax_scatter[:set_ylim](extrema(h2.x))
+end
