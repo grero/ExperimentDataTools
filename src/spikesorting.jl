c4d7e0e593b97e93677c367fd57c40aa806276de
roger <roger.herikstad@gmail.com>
2018-04-20 10:10:44 +0800

    Adds datatype for HMM SpikeTemplates

--- src/spikesorting.jl
+++ src/spikesorting.jl
@@ -38,5 +38,38 @@ end
 
 HMMSortedSpikes() = load(HMMSortedSpikes)
 
+struct SpikeTemplateArgs <: DPHT.DPHDataArgs
+end
+
+struct SpikeTemplates <: DPHT.DPHData
+    spikeshapes::Array{Float64,3}
+    cinv::Matrix{Float64}
+    pp::Vector{Float64}
+    channel::Int64
+    args::SpikeTemplateArgs
+end
+
+SpikeTemplates() = SpikeTemplates(Array{Float64}(0,0,0), Matrix{Float64}(0,0), Vector{Float64}(0), -1, SpikeTemplateArgs())
+
+DPHT.level(::Type{SpikeTemplates}) = "channel"
+DPHT.filename(::Type{SpikeTemplates}) = "spike_templates.mat"
+DPHT.datatype(::Type{SpikeTemplateArgs}) = SpikeTemplates
 
 
+function SpikeTemplates(args::SpikeTemplateArgs;kvs...)
+    fnames = glob("spike_templates.hdf5")
+    if isempty(fnames)
+        return SpikeTemplates()
+    end
+    fname = first(fnames)
+    channel = parse(Int64, DPHT.get_numbers(DPHT.get_level_name(DPHT.level(SpikeTemplates))))
+    HDF5.h5open(fname, "r") do ff
+        spikeshapes = read(ff, "spikeForms") 
+        cinv = read(ff, "cinv")
+        if ndims(cinv) == 1
+            cinv = cinv[:,1:1]
+        end
+        pp = read(ff,"p") 
+        SpikeTemplates(spikeshapes, cinv, pp, channel,args)
+    end
+end
